6cc759ab2d50b159e7027079afbcf5ae07944142
Sena <z.sena.oz@outlook.com>
2021-12-09 11:00:45 +0300

    09 Aralık

--- Project 1/script.js
+++ Project 1/script.js
@@ -1,28 +1,19 @@
-var blackSquares = document.querySelectorAll(".blackSquare");
-var whiteSquares = document.querySelectorAll(".whiteSquare");
-var whitePieces = document.querySelectorAll(".WhitePiece") ;
-var blackPieces = document.querySelectorAll(".BlackPiece");
-
 var turn = 1; //if it is 1, the turn of the game is the white pieces.
 
-
 $(".WhitePiece").click(function(event){
-    var keycode = String.fromCharCode(event.keyCode);
-
     if (turn = 0) {
-        alert("The turn of the game is the black pieces.")
+        document.getElementById("message").innerHTML = "The turn of the game is the black pieces.";
     }
 
     else {
-        PossibleMoves($(this))
-        $(this).fadeOut();
+        PossibleMoves($(this));
         return turn = 0;
     }
 });
 
 $(".BlackPiece").click(function(){
     if (turn = 1) {
-        alert("The turn of the game is the white pieces.")
+        document.getElementById("message").innerHTML = "The turn of the game is the white pieces.";
     }
     else {
         $(this).fadeOut();
@@ -30,13 +21,39 @@ $(".BlackPiece").click(function(){
     }
 });
 
-function PossibleMoves(x){
-    var colNumber = x.closest('td').attr('id');
-    var rowNumber = x.closest('tr').attr('id');
 
-    if ( x.className = 'WhitePiece'){
+function PossibleMoves(e){
+    var Y = parseInt(e.closest('tr').attr('id'));
+    var X = parseInt(e.closest('td').attr('id'));
+    var classN = e.closest('span').attr('class');
+
+    if ( classN = 'WhitePiece'){
+        var PossibleY = Y+1;
+        var PossibleX1 = X;
+        var PossibleX2 = X-1;
     }
 
     else {
+        var PossibleY = Y-1;
+        var PossibleX1 = X;
+        var PossibleX2 = X+1;
     }
+
+    document.getElementById("message").innerHTML = "Now you should select the square that is <br> where to go with double click.";
+
+    $(".blackSquare").dblclick(function(){
+        var SquareY = parseInt($(this).closest('tr').attr('id'));
+        var SquareX = parseInt($(this).closest('td').attr('id'));
+        var inner =  $(this).html();
+
+        if ( inner == "") {
+            if (SquareY == PossibleY){
+                if (SquareX == (PossibleX1 || PossibleX2)) {
+                    alert("OLDU");
+                }
+            }
+            else document.getElementById("message").innerHTML = "It is not valid move.";
+        }
+        else alert("You can't put the piece on a piece.");
+    });
 }
